bbccdecf238627329c0ba2402020e16f09193958
Erick <moralerick90@gmail.com>
2024-09-20 13:05:22 -0700

    updated files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Csh_conditions
 Practice using conditional statements in C sharp
+
+## Techniques techniques explored
+1. Demonstrate the use of if, else, and else if statements
+2. Demonstrate the use of logical operators
+3. Converting variable types
---
+++ csh_conditions/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the grade earned for the class");
+string input = Console.ReadLine();
+int grade = Int16.Parse(input);
+string letter = "";
+
+if (grade >= 90){
+    letter = "A";
+}
+else if (grade >= 80){
+    letter = "B";
+}
+else if (grade >= 70){
+    letter = "C";
+}
+else if (grade >= 60){
+    letter = "D";
+}
+else {
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade: {letter}");
